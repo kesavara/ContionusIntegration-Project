5c98154182a703fb0cbfe36432137bfed8b4645e
kesavara <42240281+kesavara@users.noreply.github.com>
2023-03-24 15:53:46 +0530

    After class changes

--- src/test/java/steps/AfterActions.java
+++ src/test/java/steps/AfterActions.java
@@ -14,13 +14,13 @@ public class AfterActions {
     @After
     public static void tearDown(Scenario scenario) {
     	
-    	WebDriver driver=SeleniumDriver.getDriver();
-    	System.out.println(scenario.isFailed());
-    	 if (scenario.isFailed()) {
-             byte[] screenshotBytes = ((TakesScreenshot) driver).getScreenshotAs(OutputType.BYTES);
-             scenario.embed(screenshotBytes, "image/png");
-          
-         }
+//    	WebDriver driver=SeleniumDriver.getDriver();
+//    	System.out.println(scenario.isFailed());
+//    	 if (scenario.isFailed()) {
+//             byte[] screenshotBytes = ((TakesScreenshot) driver).getScreenshotAs(OutputType.BYTES);
+//             scenario.embed(screenshotBytes, "image/png");
+//
+//         }
     SeleniumDriver.tearDown();
     }
 }
